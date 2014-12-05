ecfc7f0b398d28ef4b6326d8085d1869117ea08c
CharisseL <charisse8@gmail.com>
2014-12-05 10:37:01 -0600

    using angular for To Do

--- js/main.js
+++ js/main.js
@@ -24,12 +24,25 @@ angular.module('todoApp', [])
 	
 		vm.addNewTask = function(){
 		vm.tasks.push(vm.newTask);
-		vm.newTask = null;
+		vm.newTask = _freshTash();
 		};
 
 		vm.removeTodo = function(todo){
 			var index = vm.tasks.indexOf(todo);
 			vm.tasks.splice(index,1);
+		};
+		vm.newTask = _freshTask();
+		vm.priorityOptions = {
+				high: 'High',
+				medium: 'Medium',
+				low: 'Low',
+				whocares: 'Whatevs'
+		};
+
+		function _freshTask(){
+			return {
+					priority: 'low'
+			}
 		}
 	});
 
